c6ec5da26e8dbb61e17615509244900b852e961e
Basani somesh <150121746+Someshbasani@users.noreply.github.com>
2025-04-13 15:56:54 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 #import joblib
-#from sklearn.linear_model import LogisticRegression
-#from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 #from sklearn.preprocessing import LabelEncoder
 
 
