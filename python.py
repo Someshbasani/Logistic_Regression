e836ef3b36411824366305d4cd0d37035664671d
Basani somesh <150121746+Someshbasani@users.noreply.github.com>
2025-04-13 15:51:08 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 #import joblib
 #from sklearn.linear_model import LogisticRegression
-#from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+#from sklearn.preprocessing import LabelEncoder
+
 
 # Title
 st.title("Model Deployment: Logistic Regression")
